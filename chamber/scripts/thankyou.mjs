05cf0fde62b53b3d21ec0125766b04e47993801f
Fernanda-Testing <mfsouzafaria@byupathway.edu>
2025-08-09 00:01:21 -0300

    fixes thank you page

--- chamber/scripts/thankyou.mjs
+++ chamber/scripts/thankyou.mjs
@@ -4,7 +4,6 @@ document.getElementById("formTimestamp").value = new Date().toLocaleString();
 const myInfo = new URLSearchParams(window.location.search);
 
 document.querySelector("#results").innerHTML = `
-<h2>Thank you for completing the form!</h2>
 <p>Membership Application of: ${myInfo.get('first')} ${myInfo.get('last')}</p>
 <p>Business Organization's Name: ${myInfo.get('buisiness-organization-name')}</p>
 <p>Your Email: ${myInfo.get('email')}</p>
