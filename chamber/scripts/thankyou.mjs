e0060a600105ea188b6194d60ed2026c83c2b050
Fernanda-Testing <mfsouzafaria@byupathway.edu>
2025-08-09 01:37:31 -0300

    fix in my thank you mjs

--- chamber/scripts/thankyou.mjs
+++ chamber/scripts/thankyou.mjs
@@ -1,9 +1,11 @@
-const myInfo = new URLSearchParams(window.location.search);
+document.addEventListener("DOMContentLoaded", () => {
+    const myInfo = new URLSearchParams(window.location.search);
 
-document.querySelector("#results").innerHTML = `
-<p><strong>Membership Application of:</strong> ${myInfo.get('first')} ${myInfo.get('last')}</p>
-<p><strong>Business Organization's Name:</strong> ${myInfo.get('business-organization-name')}</p>
-<p><strong>Your Email:</strong> ${myInfo.get('email')}</p>
-<p><strong>Your Phone:</strong> ${myInfo.get('phone')}</p>
-<p><strong>Form completed on:</strong> ${myInfo.get('formTimestamp')}</p>
-`
+    document.querySelector("#results").innerHTML = `
+    <p><strong>Membership Application of:</strong> ${myInfo.get('first')} ${myInfo.get('last')}</p>
+    <p><strong>Business Organization's Name:</strong> ${myInfo.get('business-organization-name')}</p>
+    <p><strong>Your Email:</strong> ${myInfo.get('email')}</p>
+    <p><strong>Your Phone:</strong> ${myInfo.get('phone')}</p>
+    <p><strong>Form completed on:</strong> ${myInfo.get('formTimestamp')}</p>
+  `;
+});
