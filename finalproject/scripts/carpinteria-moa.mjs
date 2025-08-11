fb1fee696c94e204387a59928c8fdda88f851e38
Fernanda-Testing <mfsouzafaria@byupathway.edu>
2025-08-11 18:12:16 -0300

    first commit final project

---
+++ finalproject/scripts/carpinteria-moa.mjs
@@ -0,0 +1,15 @@
+const currentYear = document.querySelector("#currentyear");
+
+const hamButton = document.querySelector('#menu');
+const pages = document.querySelector('.pages');
+
+const today = new Date();
+
+currentYear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+if (hamButton && pages) {
+    hamButton.addEventListener('click', () => {
+        pages.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+}
